8b6b27a52a956a497b8a2874897ece7523ca7972
Israel Ziri <oladejoisreal274@gmail.com>
2024-01-23 16:51:36 +0100

    .

--- src/main/java/org/example/services/BuyerService.java
+++ src/main/java/org/example/services/BuyerService.java
@@ -5,6 +5,6 @@ import org.example.data.model.Art;
 import java.util.List;
 
 public interface BuyerService {
-    void purchaseArt(String email, int atrId, int amount);
+
 
 }
--- src/main/java/org/example/services/BuyerServiceImpl.java
+++ src/main/java/org/example/services/BuyerServiceImpl.java
@@ -1,9 +1,6 @@
 package org.example.services;
 
-import org.example.data.model.Art;
 import org.example.data.model.Buyer;
-import org.example.data.repository.BuyerRepository;
-import org.example.dto.request.FindByEmail;
 import org.example.dto.request.RegisterRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -13,24 +10,5 @@ import java.util.List;
 
 @Service
 public class BuyerServiceImpl implements BuyerService {
-    private AdminService adminService;
-    private final FindByEmail findByEmail = new FindByEmail();
-    private final List<RegisterRequest> registerRequests = new ArrayList<>();
-    @Autowired
-    private  BuyerService buyerService;
 
-
-    @Override
-    public void purchaseArt(String email, int artId, int amount) {
-            Buyer buyer = getBuyerByEmail(email);
-            if (buyer != null && buyer.isEnable()) {
-
-        }
-
-    }
-
-
-    private Buyer getBuyerByEmail(String email) {
-        return (Buyer) findByEmail.findByEmail(email);
-    }
 }
