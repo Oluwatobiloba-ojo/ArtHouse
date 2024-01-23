f92bb7c9abec53cac139816bc644043fa52d3226
Israel Ziri <oladejoisreal274@gmail.com>
2024-01-23 16:54:48 +0100

    .

--- src/main/java/org/example/services/BuyerService.java
+++ src/main/java/org/example/services/BuyerService.java
@@ -1,10 +1,11 @@
 package org.example.services;
 
+
 import org.example.data.model.Art;
 
 import java.util.List;
 
 public interface BuyerService {
-
+List<Art> viewAllArt();
 
 }
--- src/main/java/org/example/services/BuyerServiceImpl.java
+++ src/main/java/org/example/services/BuyerServiceImpl.java
@@ -1,14 +1,17 @@
 package org.example.services;
 
-import org.example.data.model.Buyer;
-import org.example.dto.request.RegisterRequest;
-import org.springframework.beans.factory.annotation.Autowired;
+
+import org.example.data.model.Art;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
+
 
 @Service
 public class BuyerServiceImpl implements BuyerService {
 
+    @Override
+    public List<Art> viewAllArt() {
+        return null;
+    }
 }
