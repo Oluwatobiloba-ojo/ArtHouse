b052cbbae5b5c9abe20cf85b0e34eb5f0f709fd1
delighted <deborahdelighted5@gmail.com>
2024-01-23 15:44:49 +0100

    buyer service work

--- src/main/java/org/example/services/BuyerService.java
+++ src/main/java/org/example/services/BuyerService.java
@@ -1,10 +1,12 @@
 package org.example.services;
 
 import org.example.data.model.Art;
+import org.example.data.model.Buyer;
 
 import java.util.List;
 
 public interface BuyerService {
     List<Art> viewAllPublishedArt(String email);
+    Buyer findBuyerByEmail(String email);
 
 }
